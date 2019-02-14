5d92d7088d96845ad62677f0e29a5d25fe239d44
Mohit Sharma <sharma-mohit1@hotmail.com>
2019-02-15 00:48:28 +0530

    sure commit

---
+++ src/test/java/com/profile/AppTest.java
@@ -0,0 +1,22 @@
+package com.profile;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+      System.out.println("Hello Mannu");
+        assertTrue( true );
+    }
+    
+}
